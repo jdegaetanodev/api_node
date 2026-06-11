f001711459713e4bdde36ef1a707efb1447fa481
valenporta <valentinporta1@hotmail.com>
2026-06-10 21:33:11 -0300

    dtos especialidades

--- servicios/especialidades.servicio.js
+++ servicios/especialidades.servicio.js
@@ -6,11 +6,19 @@ export const obtenerTodos = async () =>
 export const obtenerUno = async (id) =>
   pool.execute('SELECT * FROM especialidades WHERE activo = 1 AND id_especialidad = ?', [id]);
 
-export const crear = async (nombre) =>
+export const crear = async ({ nombre }) =>
   pool.execute('INSERT INTO especialidades (nombre) VALUES (?)', [nombre]);
 
-export const actualizar = async (nombre, id) =>
-  pool.execute('UPDATE especialidades SET nombre = ? WHERE id_especialidad = ? AND activo = 1', [nombre, id]);
+export const actualizar = async ({ nombre }, id) =>
+  pool.execute(
+    'UPDATE especialidades SET nombre = ? WHERE id_especialidad = ? AND activo = 1',
+    [nombre, id]
+  );
 
 export const eliminar = async (id) =>
-  pool.execute('UPDATE especialidades SET activo = 0 WHERE id_especialidad = ? AND activo = 1', [id]);
+  pool.execute(
+    'UPDATE especialidades SET activo = 0 WHERE id_especialidad = ? AND activo = 1',
+    [id]
+  );
+
+  
